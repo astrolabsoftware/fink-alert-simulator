0f6af07e9dc523b4f5903545f249a84dd87c8fd1
Fabrice Jammes <fjammes@users.noreply.github.com>
2023-12-18 15:53:23 +0100

    Run ciux-based CI (#28)

--- conf.sh
+++ conf.sh
@@ -1,10 +1,9 @@
+. "$CIUXCONFIG"
+
 # Container image
-GIT_HASH="$(git -C $DIR describe --dirty --always)"
-TAG="$GIT_HASH"
+TAG="$FINK_ALERT_SIMULATOR_VERSION"
 IMAGE="gitlab-registry.in2p3.fr/astrolabsoftware/fink/fink-alert-simulator:$TAG"
 
 # Used for development purpose (i.e. with telepresence)
 DEV_IMAGE="astrolabsoftware/fink/fink/fink-alert-simulator-devps"
 
-# Used for integration tests
-FINK_BROKER_DIR="/tmp/fink-broker"
