3637bba02706a674ca58bdbb4d11e1fb9378a727
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-08-09 12:38:00 +0200

    Improve image management WIP

--- push-image.sh
+++ push-image.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
-# Push image to remote container registry or load it inside kind
+# Push image to Docker Hub or load it inside kind
 
 # @author  Fabrice Jammes, IN2P3
 
 set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
-. "$DIR/conf.sh"
-
-IMAGE="$IMAGE"
 
 set -e
 
@@ -21,21 +18,21 @@ Usage: `basename $0` [options] path host [host ...]
   Available options:
     -h          this message
     -k          development mode: load image in kind
-    -d          do not push image to docker hub
+    -d          do not push image to remote registry
 
-Push image to container rgistry and/or load it inside kind
+Push image to remote registry and/or load it inside kind
 EOD
 }
 
 kind=false
-dockerhub=true
+registry=true
 
 # get the options
 while getopts dhk c ; do
     case $c in
 	    h) usage ; exit 0 ;;
 	    k) kind=true ;;
-	    d) dockerhub=false ;;
+	    d) registry=false ;;
 	    \?) usage ; exit 2 ;;
     esac
 done
@@ -46,9 +43,11 @@ if [ $# -ne 0 ] ; then
     exit 2
 fi
 
+$(ciux get image --check $DIR --env)
+
 if [ $kind = true ]; then
-  kind load docker-image "$IMAGE"
+  kind load docker-image "$CIUX_IMAGE_URL"
 fi
-if [ $dockerhub = true ]; then
-  docker push "$IMAGE"
+if [ $registry = true ]; then
+  docker push "$CIUX_IMAGE_URL"
 fi
